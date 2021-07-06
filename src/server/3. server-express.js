5ff3555d5a11a5aa9bca3eff0b88baebf841650f
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-07-06 20:31:39 +0100

    use array [] in adding multiple URL choices

--- src/server/3. server-express.js
+++ src/server/3. server-express.js
@@ -1,7 +1,4 @@
-// ATTEMPTED EXPRESS-SERVER - WORKING
-// Utilized res.redirect to redirect from /about-us to /about
-// Not yet found conditional syntax to add two or more URL choices to "/"
-// like I could do with SWITCH CASEs and IFs.
+// EXPRESS-SERVER - WORKING
 
 const express = require("express");
 const app = express();
@@ -9,11 +6,8 @@ const path = require("path");
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) =>
-  res.status(200).sendFile(path.join(__dirname, "public", "index.html"))
-);
-
-app.get("/home", (req, res) =>
+// use array [] to add two or more URL choices - Yippee!!!
+app.get(["/", "/home"], (req, res) =>
   res.status(200).sendFile(path.join(__dirname, "public", "index.html"))
 );
 
@@ -21,9 +15,8 @@ app.get("/about", (req, res) =>
   res.status(200).sendFile(path.join(__dirname, "public", "about.html"))
 );
 
-app.get("/about-us", (req, res) =>
-  res.redirect("/about")
-);
+// Utilized res.redirect to redirect from /about-us to /about
+app.get("/about-us", (req, res) => res.redirect("/about"));
 
 app.get("/contact", (req, res) =>
   res.status(200).sendFile(path.join(__dirname, "public", "contact.html"))
